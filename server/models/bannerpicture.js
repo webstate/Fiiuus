f10de726541c3611c3df71c7d1a05eb08535eb5d
Rene T <renetsek@gmail.com>
2018-11-27 11:07:56 +0200

    Rename bannerImage model @server

--- server/models/bannerpicture.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var bannerPicture = new Schema({
-    picturePath:String,
-    block: String,
-})
-
-module.exports = mongoose.model('bannerpicture', bannerPicture);
