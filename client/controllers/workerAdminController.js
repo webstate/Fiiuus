6c83aed2c5722e0653a4d92ad54faa68e8d4a6b9
Rene T <renetsek@gmail.com>
2019-02-01 15:26:18 +0200

    Add optimized image functionality in admin worker controller

--- client/controllers/workerAdminController.js
+++ client/controllers/workerAdminController.js
@@ -24,27 +24,60 @@ workerAdminController.controller('workerAdminController', function($scope, worke
         angular.forEach($scope.files, function(file){
             fd.append('file', file);
         })
-        pictureService.saveImage(fd).then(function(file){
-            $scope.picture = file;
-            $(".picture-holder").css({"background-image": "url("+file+")", "border": "none"});
-        }, function(err){
-            console.log(err);
-        })
-    }
-    $scope.submitWorker = function(){
-        workerService.addWorker($scope.worker.name, $scope.worker.occupationEst, $scope.worker.email, $scope.worker.infoEst, $scope.picture,
-             $scope.worker.infoEng, $scope.worker.occupationEng, $scope.worker.infoFin, $scope.worker.occupationFin,
-             $scope.worker.infoRus, $scope.worker.occupationRus).then(function(data){
-            $scope.worker = {};
-            workerService.getWorkers().then(function(data){
-                $scope.workers = data;
-            }, function(err){
-                console.log(err);
+        pictureService.saveImage(fd).then(function(path){
+
+            pictureService.compress().then(function() {
+                if(path.indexOf('/')>=0 && path.indexOf('/') != -1){
+                    var splitstr = path.split("/");
+                } else {
+                    var splitstr = path.split(/(\u005C)/g);
+                }
+                var correctPath = 'uploads/test/'+ splitstr[splitstr.length - 1];
+                var optPath = 'uploads/compressed/'+ splitstr[splitstr.length - 1];
+                // var added = new Date(Date.now()).toLocaleString();
+
+                $scope.picture = correctPath;
+                $scope.optPath = optPath;
+
+                // $(".picture-holder").css({"background-image": "url(" + path + ")", "border": "none"});
+                $(".picture-holder").css({"background-image": "url(" + optPath + ")", "border": "none"});
+                // $(".picture-holder").css({"background-image": "url(" + picture + ")", "border": "none"});
+
             })
+            // $scope.picture = path;
+
         }, function(err){
             console.log(err);
         })
     }
+
+    /* Add new worker */
+    $scope.submitWorker = function(){
+        workerService.addWorker(
+            $scope.worker.name,
+            $scope.worker.occupationEst,
+            $scope.worker.email,
+            $scope.worker.infoEst,
+            $scope.picture,
+            $scope.optPath,
+            $scope.worker.infoEng,
+            $scope.worker.occupationEng,
+            $scope.worker.infoFin,
+            $scope.worker.occupationFin,
+            $scope.worker.infoRus,
+            $scope.worker.occupationRus)
+            .then(function(data){
+                $scope.worker = {};
+                workerService.getWorkers().then(function(data){
+                    $scope.workers = data;
+                }, function(err){
+                    console.log(err);
+                })
+            }, function(err){
+                    console.log(err);
+        })
+    }
+
     $scope.removeWorker = function(id){
         workerService.removeWorker(id).then(function(data){
             workerService.getWorkers().then(function(data){
@@ -56,6 +89,7 @@ workerAdminController.controller('workerAdminController', function($scope, worke
             console.log(err);
         })
     }
+
     $scope.showEst = function(){
 
         $scope.estModal = true;
@@ -134,6 +168,7 @@ workerAdminController.controller('workerAdminController', function($scope, worke
             $scope.editOccupationRus = data.occupationRus;
             $scope.editInfoRus = data.infoRus;
             $scope.picture = data.picture;
+            $scope.optPath = data.optPath;
         })
     }
     $scope.updateWorker = function(){
@@ -148,6 +183,7 @@ workerAdminController.controller('workerAdminController', function($scope, worke
         var rusOccupation = "";
         var rusInfo = "";
         var picture = "";
+        var optPath = "";
 
         estName = $scope.editNameEst;
         estOccupation = $scope.editOccupationEst;
@@ -160,6 +196,7 @@ workerAdminController.controller('workerAdminController', function($scope, worke
         rusOccupation = $scope.editOccupationRus;
         rusInfo = $scope.editInfoRus;
         picture = $scope.picture;
+        optPath = $scope.optPath;
 
         if($scope.editNameEst === "{{nameEditEst}}"){
             estName = $scope.nameEditEst;
@@ -192,16 +229,26 @@ workerAdminController.controller('workerAdminController', function($scope, worke
             rusInfo = $scope.infoEditRus;
         }
 
-        workerService.updateWorker($scope.workerId, estName, estOccupation, email, estInfo,
-            engOccupation, engInfo, finOccupation, finInfo, rusOccupation, rusInfo, picture).then(function(data){
-            workerService.getWorkers().then(function(data){
+        workerService.updateWorker(
+            $scope.workerId,
+            estName,
+            estOccupation,
+            email,
+            estInfo,
+            engOccupation,
+            engInfo,
+            finOccupation,
+            finInfo,
+            rusOccupation,
+            rusInfo,
+            picture,
+            optPath)
+            .then(function(data){
+            workerService.getWorkers({ cache: false }).then(function(data){
                 $scope.workers = data;
             }, function(err){
                 console.log(err);
             })
         })
-
     }
-
-
 })
